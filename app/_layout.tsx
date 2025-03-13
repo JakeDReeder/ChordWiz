a543576152818069d24bb89847a2ec870446cf18
Jake Reeder <jacobdreeder@gmail.com>
2025-03-13 16:47:36 -0400

    Changed to tab layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,38 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
 import React from "react";
-import { Image, StyleSheet } from "react-native";
-
+import { Image } from "react-native";
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { Colors } from "@/constants/Colors";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const logo = "@/assets/images/ChordWiz_transparent.png";
 
   return (
-    <Stack>
-      <Stack.Screen
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+      }}
+    >
+      <Tabs.Screen
         name="index"
         options={{
           headerTitle: () => (
             <Image
-              source={require(logo)}
+              source={require("@/assets/images/ChordWiz_transparent.png")}
               style={{ width: 150 }}
               resizeMode="contain"
             />
           ),
-          headerTitleAlign: "left",
           headerStyle: { backgroundColor: "#252525" },
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? "home" : "home-outline"}
+              color={"white"}
+            />
+          ),
         }}
       />
-    </Stack>
+    </Tabs>
   );
 }
