e9335319f87c19dc2f169a658bc2f467c78fdd64
Jake Reeder <jacobdreeder@gmail.com>
2025-03-13 14:18:04 -0400

    Working on logo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,40 @@
 import { Stack } from "expo-router";
 import React from "react";
-import { Platform } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 
-import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+  const logo = "@/assets/images/ChordWiz_transparent.png";
+
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerTitle: () => (
+            <View style={styles.logoContainer}>
+              <Image source={require(logo)} style={styles.logo} />
+            </View>
+          ),
+          headerTitleAlign: "left",
+          headerStyle: { backgroundColor: "#252525" },
+        }}
+      />
+    </Stack>
+  );
 }
+
+const styles = StyleSheet.create({
+  logoContainer: {
+    flex: 1,
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    height: "100%",
+    resizeMode: "contain",
+  },
+});
